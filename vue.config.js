36f3a86a34df80607a9de3d2aa35df988e967800
鹹魚VisionYi <scps950613@gmail.com>
2018-06-12 20:38:38 +0800

    scss 路徑修改

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,10 @@
-const fs = require('fs');
-
 module.exports = {
   baseUrl: './',
   css: {
     loaderOptions: {
       sass: {
         // 自動載入常用的檔案，例: 全局變數、常用函數
-        data: fs.readFileSync('src/assets/scss/_variables.scss', 'utf-8'),
+        data: '@import "@/assets/scss/_variables.scss";',
       },
     },
   },
